79372d9ae281d038e6348460c6de8344d1ede39c
a-wtnbe <ayawatanabe714@gmail.com>
2021-08-29 15:59:14 +0900

    4-en-end

--- java/wcp/Chapter04.java
+++ java/wcp/Chapter04.java
@@ -1,9 +1,11 @@
 
 public class Chapter04 {
 	public static void main(String[] args) {
-		int num1;
-		String str1;
-		num1 = 467;
-		str1 = "aya";
+		int lucky_number = 467;
+		String str = "aya";
+		System.out.println(str);
+		str = "国を想う気持ちが国を滅ぼすんだ";
+		System.out.println(str);
+		System.out.println(lucky_number);
 	}
 }
