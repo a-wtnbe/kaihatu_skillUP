3864bd44473ac035602e38b641e4fc453f5fca9c
a-wtnbe <ayawatanabe714@gmail.com>
2021-09-17 21:57:05 +0900

    for1

---
+++ python/for1.py
@@ -0,0 +1,10 @@
+# 正整数 n と、 n 個の整数 a_1, ..., a_n が半角スペース区切りで与えられます。
+# 与えられた整数の中に 3 の倍数がいくつあるかを数え、出力してください。
+
+n = int(input("n"))
+a = input("a").split()
+ans = 0
+for i in range(n):
+  if int(a[i]) % 3 == 0:
+    ans += 1
+print(ans)
