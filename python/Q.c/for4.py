ff592a9212145725c78aa87a29549f216553ff6a
a-wtnbe <ayawatanabe714@gmail.com>
2021-09-18 09:40:30 +0900

    for4_me

---
+++ python/Q.c/for4.py
@@ -0,0 +1,20 @@
+# m 個の文字 c_1, ..., c_m と、 n 個の文字列 S_1, ..., S_n が与えられます。
+# 各 c_i （1 ≤ i ≤ m） について、各 S_j （1 ≤ j ≤ n） に c_i が出現するかをそれぞれ調べ、
+# 出現する場合は "YES" を、そうでない場合には "NO" を、そのつど出力してください。
+
+# me ↓
+# m = int(input())
+# for i in range(m):
+#     c = input().split()
+#     for o in range(i):
+#         s = input()
+#         if c == s:
+#             print("YES")
+#         else:
+#             print("NO")
+
+# わからん。そもそも、「Wrong Answer」になってしもうた。
+
+
+
+# ans ↓
